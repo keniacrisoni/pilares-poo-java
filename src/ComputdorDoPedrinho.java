c8a4ad804ba3441f2255204499682797715c56f0
Kenia Crisoni <keniafcrisoni@gmail.com>
2024-04-07 11:45:30 -0300

    Praticando o Polimorfismo

--- src/ComputdorDoPedrinho.java
+++ src/ComputdorDoPedrinho.java
@@ -1,21 +1,21 @@
 public class ComputdorDoPedrinho {
     public static void main(String[] args) {
 
-        System.out.println("MSN");
-        ServicoDeMensagemInstantanea msn = new MSNMessenger();
-        msn.enviarMensagem();
-        msn.receberMensagem();
+        ServicoDeMensagemInstantanea smi = null;
 
-        System.out.println("Facebook");
-        ServicoDeMensagemInstantanea fcb = new FacebookMessenger();
-        fcb.enviarMensagem();
-        fcb.receberMensagem();
+        String appEscolhido = "tlm";
 
-        System.out.println("Telegram");
-        ServicoDeMensagemInstantanea tlm = new TelegramMessenger();
-        tlm.enviarMensagem();
-        tlm.receberMensagem();
+        if (appEscolhido.equals("msn"))
+            smi = new MSNMessenger();
 
+        else if (appEscolhido.equals("fbm"))
+            smi = new FacebookMessenger();
+
+        else if (appEscolhido.equals("tlm"))
+            smi = new TelegramMessenger();
+
+        smi.enviarMensagem();
+        smi.receberMensagem();
     }
 
 }
